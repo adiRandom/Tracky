c89e0182bad49dc9cb16af612b1a10f9827d2a76
Adrian Pascu <me.adi.hf@gmail.com>
2020-08-12 17:58:12 +0300

    Endpoint change

--- app/src/main/java/com/adi_random/tracky/api/Endpoints.kt
+++ app/src/main/java/com/adi_random/tracky/api/Endpoints.kt
@@ -4,4 +4,4 @@ package com.adi_random.tracky.api
  * Created by Adrian Pascu on 7/5/2020.
  */
 
-const val SEARCH_BOOK_ENDPOINT = "http://10.0.2.2:5001/tracky-f2b0c/europe-west1/"
+const val SEARCH_BOOK_ENDPOINT = "https://europe-west1-tracky-f2b0c.cloudfunctions.net/"
